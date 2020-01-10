df746279f57731424fd4315ac350007ccd5f9985
Creative Penguin <panda501wp@gmail.com>
2020-01-10 02:22:56 -0500

    I just worked on this for way too long, and have no idea why it's still not working

--- control.c
+++ control.c
@@ -14,24 +14,23 @@
 #define VIEW_ARG "-v"
 #define REMOVE_ARG "-r"
 #define SHM_KEY 25694
-#define SEM_KEY 63419
+#define SEM_KEY 24601
 #define SEG_SIZE 255
 
 union semun {
-  int              val;    /* Value for SETVAL */
-  struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
-  unsigned short  *array;  /* Array for GETALL, SETALL */
-  struct seminfo  *__buf;  /* Buffer for IPC_INFO
-                              (Linux-specific) */
+	int              val;    /* Value for SETVAL */
+	struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+	unsigned short  *array;  /* Array for GETALL, SETALL */
+	struct seminfo  *__buf;	 /* Buffer for IPC_INFO
+	                            (Linux-specific) */
 };
 
 int main(int argc, char** argv) {
   int shmd;
   char *data;
   int semd;
-  int v;
-  int r;
   int newfile;
+  FILE *file;
   struct sembuf sb;
   //printf("argv: %s\n", argv[1]);
   if(argc == 1) {
@@ -40,49 +39,61 @@ int main(int argc, char** argv) {
   }
 
   if(strcmp(argv[1], CREATE_ARG) == 0) {
-    shmd = shmget(SHM_KEY, SEG_SIZE, IPC_CREAT | IPC_EXCL | 0640);
+    shmd = shmget(SHM_KEY, 1, IPC_CREAT | IPC_EXCL | 0640);
     data = shmat(shmd, 0, 0);
-    printf("shared memory created\n");
-    if(semd == -1) {
-      semd = semget(SEM_KEY, 1, 0);
-      v = semctl(semd, 0, GETVAL, 0);
+    printf("shared memory created\n", shmd);
+    if(semd < 0) {
+      semd = semget(SHM_KEY, 1, 0);
+      semctl(semd, 0, GETVAL, 0);
     } else {
       union semun us;
       us.val = 1;
-      r = semctl(semd, 0, SETVAL, us);
+      semctl(semd, 0, SETVAL, us);
     }
-    printf("semaphore created\n");
-    newfile = open("ban.out", O_CREAT | O_EXCL | O_TRUNC | O_RDONLY, 0640);
+    printf("semaphore created, semd = %d\n", semd);
+    //newfile = open("ban.out", O_CREAT | O_EXCL | O_TRUNC | O_RDONLY, 0640);
+    fopen("ban.out", "w+");
+    file = fopen("ban.out", "r");
+    printf("%d\n", file);
     printf("file created\n");
   }
 
   if(strcmp(argv[1], VIEW_ARG) == 0) {
-    printf("Trying to get in...\n");
-    sb.sem_num = 0;
-    sb.sem_op = -1;
-    semop(semd, &sb, 1);
-    if(semd == -1) {
-      printf("could not get in\n");
-      exit(0);
-    }
+    //printf("Trying to get in...\n");
+    //sb.sem_num = 1;
+    //sb.sem_op = -1;
+    //printf("semd = %d, semop = %d\n", semd, semop(semd, &sb, 1));
+    //if (semd == -1) {
+    //  printf("could not get in\n");
+    //  exit(0);
+    //}
+
     printf("The story so far:\n");
     //strcpy(data, "hello there\n");
     //printf("%s\n", strchr(data, '\n'));
     //data[strchr(data, "\n")] = 0;
-    char *buf;
-    //while(read(newfile, buf, SEG_SIZE)) printf("%s", buf);
-    read(newfile, buf, SEG_SIZE);
+    //char *buf;
+    char buf[255];
+
+    //while(read(newfile, buf, SEG_SIZE) != -1) printf("%s\n", buf);
+    fgets(buf, SEG_SIZE, file);
     printf("%s\n", buf);
-    sb.sem_num = 0;
-    sb.sem_op = 1;
-    semop(semd, &sb, 1);
+    //while((buf = fgetc(file)) != EOF) {
+    //  printf("%c", buf);
+    //}
+    //printf("%s\n", buf);
+
+    //sb.sem_num = 1;
+    //sb.sem_op = 1;
+    //semop(semd, &sb, 1);
   }
 
   if(strcmp(argv[1], REMOVE_ARG) == 0) {
     printf("trying to get in\n");
     sb.sem_num = 0;
     sb.sem_op = -1;
-    semop(semd, &sb, 1);
+    if(semop(semd, &sb, 1) == -1) printf("semop = -1");
+    printf("semd = %d\n", semd);
     if(semd == -1) {
       printf("could not get in\n");
       exit(1);
@@ -90,9 +101,11 @@ int main(int argc, char** argv) {
     shmdt(data);
     shmctl(SHM_KEY, IPC_RMID, 0);
     printf("shared memory removed\n");
-    semctl(SEM_KEY, IPC_RMID, 0);
+    semctl(SEM_KEY, 0, IPC_RMID);
+    //shmctl(int __shmid, int __cmd, struct shmid_ds *__buf);
+    //semctl(int __semid, int __semnum, int __cmd, ...);
     printf("semaphore memory removed\n");
-    semop(semd, &sb, 1);
+    //semop(semd, &sb, 1);
     close(newfile);
   }
 
--- write.c
+++ write.c
@@ -9,41 +9,50 @@
 
 #define WRITE 1
 #define SHM_KEY 25694
-#define SEM_KEY 63419
+#define SEM_KEY 24601
 #define SEG_SIZE 255
 
 union semun {
-  int val;               /* Value for SETVAL */
-  struct semid_ds *buf;  /* Buffer for IPC_STAT, IPC_SET */
-  unsigned short *array; /* Array for GETALL, SETALL */
-  struct seminfo *__buf; /* Buffer for IPC_INFO
-                            (Linux-specific) */
+	int              val;    /* Value for SETVAL */
+	struct semid_ds *buf;    /* Buffer for IPC_STAT, IPC_SET */
+	unsigned short  *array;  /* Array for GETALL, SETALL */
+	struct seminfo  *__buf;	 /* Buffer for IPC_INFO
+                                (Linux-specific) */
 };
 
 int main() {
-  int shmd = shmget(SHM_KEY, 1, 0);
-  int semd = semget(SEM_KEY, 1, 0);
-  char *data = shmat(shmd, 0, 0);
-  int file = open("ban.out", O_RDWR, 0640);
-  struct sembuf sb;
-  // int fds[2];
-  // pipe(fds);
-  // char term_in[255];
-  sb.sem_num = 0;
-  sb.sem_op = -1;
-  sb.sem_flg = SEM_UNDO;
-  if(shmd == -1) {
-    printf("Run ./control -c first\n");
-    printf("There is no shared memory available\n");
-  }
-  printf("Last addition:");
-  printf("%s\n", data);
-  printf("Your addition:");
-  fgets(data, SEG_SIZE, stdin);
-  write(file, data, SEG_SIZE);
-  close(file);
-  // term_in[strlen(term_in) - 1] = '\0';
-  // write(fds[WRITE], term_in, 255);
-  // printf("fds[WRITE] = %d\n", fds[WRITE]);
-  printf("data = %s\n", data);
+	int semd = semget(SHM_KEY, 1, 0);
+  printf("semd = %d, ", semd);
+	//int file = open("ban.out", O_APPEND, 0640);
+  FILE *file1 = fopen("ban.out", "a");
+	struct sembuf sb;
+	// int fds[2];
+	// pipe(fds);
+	// char term_in[255];
+	sb.sem_num = 1;
+	sb.sem_op = -1;
+	sb.sem_flg = SEM_UNDO;
+  if(semop(semd, &sb, 1) == -1) printf("semop == -1\n");
+  //if(semd == -1) {
+	//	printf("Run ./control -c first\n");
+	//	printf("There is no shared memory available\n");
+  //  exit(1);
+	//}
+	int shmd = shmget(SHM_KEY, 1, 0);
+  printf("shmd = %d\n", shmd);
+	char *data = shmat(shmd, 0, 0);
+	printf("Last addition:");
+	printf("%s\n", data);
+	printf("Your addition:");
+	fgets(data, SEG_SIZE, stdin);
+	fputs(data, file1);
+  //write(file, data, SEG_SIZE);
+	//close(file);
+  fclose(file1);
+  shmdt(data);
+  return 0;
+	// term_in[strlen(term_in) - 1] = '\0';
+	// write(fds[WRITE], term_in, 255);
+	// printf("fds[WRITE] = %d\n", fds[WRITE]);
+	//printf("data = %s\n", data);
 }
